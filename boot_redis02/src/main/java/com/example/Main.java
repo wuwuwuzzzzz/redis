744debdc0f2563d7b00923c956aa1906aea9b973
wxz <759126349@qq.com>
2022-12-21 17:08:26 +0800

    优化代码

--- boot_redis02/src/main/java/com/example/Main.java
+++ boot_redis02/src/main/java/com/example/Main.java
@@ -1,5 +1,10 @@
 package com.example;
 
+/**
+ *
+ * @author wxz
+ * @date 17:05 2022/12/21
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
