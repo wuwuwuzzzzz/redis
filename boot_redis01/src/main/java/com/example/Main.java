7de18ee810a41e5163daaa15bd1b627283f68936
wxz <759126349@qq.com>
2022-12-21 18:16:17 +0800

    优化代码

--- boot_redis01/src/main/java/com/example/Main.java
+++ boot_redis01/src/main/java/com/example/Main.java
@@ -1,10 +1,13 @@
 package com.example;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  *
  * @author wxz
  * @date 17:06 2022/12/21
  */
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
---
+++ boot_redis01/src/main/java/com/example/config/RedisConfig.java
@@ -0,0 +1,13 @@
+package com.example.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author wxz
+ * @date 17:32 2022/12/21
+ */
+@Configuration
+public class RedisConfig {
+
+}
