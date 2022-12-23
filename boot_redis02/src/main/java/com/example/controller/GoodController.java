1b336dcd8a101c2a0ce9294ae117f132f489de64
wxz <759126349@qq.com>
2022-12-23 20:08:05 +0800

    优化代码

--- boot_redis02/src/main/java/com/example/controller/GoodController.java
+++ boot_redis02/src/main/java/com/example/controller/GoodController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.UUID;
 
 /**
  *
@@ -14,6 +15,8 @@ import javax.annotation.Resource;
  */
 @RestController
 public class GoodController {
+
+    public static final String REDIS_LOCK = "wxzLock";
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
@@ -29,16 +32,30 @@ public class GoodController {
      */
     @GetMapping("/buyGoods")
     public String buyGoods() {
-        String s = stringRedisTemplate.opsForValue().get("goods:001");
-        int goodNumber = s == null ? 0 : Integer.parseInt(s);
-        if (goodNumber > 0) {
-            int realNumber = goodNumber - 1;
-            stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
-            System.out.println("成功买到商品，库存还剩下" + realNumber + "件" + "\t 服务端口 " + serverPort);
-            return "成功买到商品，库存还剩下" + realNumber + "件" + "\t 服务端口 " + serverPort;
-        } else {
-            System.out.println("商品已经售完/活动结束/调用超时，欢迎下次光临" + "\t 服务端口 " + serverPort);
-            return "商品已经售完/活动结束/调用超时，欢迎下次光临" + "\t 服务端口 " + serverPort;
+
+        String value = UUID.randomUUID() + Thread.currentThread().getName();
+        try {
+            // setNX
+            Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value);
+
+            if (Boolean.FALSE.equals(flag)) {
+                return "抢锁失败!";
+            }
+
+            String s = stringRedisTemplate.opsForValue().get("goods:001");
+            int goodNumber = s == null ? 0 : Integer.parseInt(s);
+
+            if (goodNumber > 0) {
+                int realNumber = goodNumber - 1;
+                stringRedisTemplate.opsForValue().set("goods:001", String.valueOf(realNumber));
+                System.out.println("成功买到商品，库存还剩下" + realNumber + "件" + "\t 服务端口 " + serverPort);
+                return "成功买到商品，库存还剩下" + realNumber + "件" + "\t 服务端口 " + serverPort;
+            } else {
+                System.out.println("商品已经售完/活动结束/调用超时，欢迎下次光临" + "\t 服务端口 " + serverPort);
+                return "商品已经售完/活动结束/调用超时，欢迎下次光临" + "\t 服务端口 " + serverPort;
+            }
+        } finally {
+            stringRedisTemplate.delete(REDIS_LOCK);
         }
     }
 }
