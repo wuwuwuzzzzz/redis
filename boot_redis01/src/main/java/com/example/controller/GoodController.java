cb650921865bff270af500e2ac5c8ca7c776c876
wxz <759126349@qq.com>
2022-12-23 14:49:32 +0800

    优化代码

---
+++ boot_redis01/src/main/java/com/example/controller/GoodController.java
@@ -0,0 +1,12 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author wxz
+ * @date 14:40 2022/12/23
+ */
+@RestController
+public class GoodController {
+}
