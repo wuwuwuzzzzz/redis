e0563c4e5012a43cba7de911e495aab0ab13e8b0
wxz <759126349@qq.com>
2022-12-24 20:25:49 +0800

    redisson

--- boot_redis02/src/main/java/com/example/controller/GoodController.java
+++ boot_redis02/src/main/java/com/example/controller/GoodController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.utils.RedisUtils;
 import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,6 +45,9 @@ public class GoodController {
     public String buyGoods() throws Exception {
 
         String value = UUID.randomUUID() + Thread.currentThread().getName();
+        RLock redissonLock = redisson.getLock(REDIS_LOCK);
+        redissonLock.lock();
+
         try {
             // setNX
             Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDIS_LOCK, value, 10L, TimeUnit.SECONDS);
@@ -65,25 +69,7 @@ public class GoodController {
                 return "商品已经售完/活动结束/调用超时，欢迎下次光临" + "\t 服务端口 " + serverPort;
             }
         } finally {
-            Jedis jedis = RedisUtils.getJedis();
-            String script = "if redis.call(\"get\",KEYS[1]) == ARGV[1]\n" +
-                    "then\n" +
-                    "    return redis.call(\"del\",KEYS[1])\n" +
-                    "else\n" +
-                    "    return 0\n" +
-                    "end";
-            try {
-                Object o = jedis.eval(script, Collections.singletonList(REDIS_LOCK), Collections.singletonList(value));
-                if (LUA_NUMBER.equals(o.toString())) {
-                    System.out.println("------ del redis lock ok");
-                } else {
-                    System.out.println("------ del redis lock error");
-                }
-            } finally {
-                if (null != jedis) {
-                    jedis.close();
-                }
-            }
+            redissonLock.unlock();
         }
     }
 }
